7dc59f9b301d12bfc0aef19dfcb0d7287ce0dd90
Robert Petersen <robert.petersen808@gmail.com>
2022-08-04 14:15:15 -0500

    cleaned repo, added styles, some js done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [active, setActive] = useState(1);
+  const [prevDisabled, setPrevDisabled] = useState(true);
+  const [nextDisabled, setNextDisabled] = useState(false);
+
+  const disableCheck = (active, button) => {
+    if (button === "prev") {
+      if (active === 2) {
+        setPrevDisabled(true);
+        setActive(active - 1);
+      } if (active === 4) {
+        setNextDisabled(false);
+        setActive(active - 1);
+      } else {
+        setActive(active - 1);
+      }
+    } else if (button === "next") {
+      if (active === 3) {
+        setNextDisabled(true);
+        setActive(active + 1);
+      } if (active === 1) {
+        setPrevDisabled(false);
+        setActive(active + 1);
+      } else {
+        setActive(active + 1);
+      }
+    } else {
+      console.log("Error in 'disableCheck' function");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="progressContainer">
+          <div className="progress" id="progress"></div>
+          <div 
+            className={active === 1 ? "circle active" : "circle"}
+          >1</div>
+          <div 
+            className={active === 2 ? "circle active" : "circle"}
+          >2</div>
+          <div 
+            className={active === 3 ? "circle active" : "circle"}
+          >3</div>
+          <div 
+            className={active === 4 ? "circle active" : "circle"}
+          >4</div>
+        </div>
+
+        <button 
+          className="btn" 
+          id="prev" 
+          disabled={prevDisabled}
+          onClick={() => {disableCheck(active, "prev")}}
+        >Prev</button>
+        <button 
+          className="btn" 
+          id="next" 
+          disabled={nextDisabled}
+          onClick={() => {disableCheck(active, "next")}}
+        >Next</button>
+      </div>
     </div>
   );
 }
